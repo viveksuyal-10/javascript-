f0607979e2b81a64f1550a3b8592ee9b9af88fcf
viveksuyal-10 <124768775+viveksuyal-10@users.noreply.github.com>
2024-04-19 04:55:11 +0000

    datatypes

---
+++ data.js
@@ -0,0 +1,12 @@
+"use strict";
+let name = "vivek";
+let age = 19;
+let isLoggedin = true;
+// number
+// string
+// boolean
+// null = standalone value
+// undefined = value not defiend but variable declared
+// symbol = unique
+console.log(typeof(null));// object
+console.log(typeof(undefined));//undefined
---
+++ var.js
@@ -0,0 +1,4 @@
+const accountId = 122333;
+let accountEmail = "vivek10@gmail.com";
+var accountPassword = "12345";
+accountCity = "delhi";
