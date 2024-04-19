a1a7714c79d3fad954ac9c223473734fa332a9f7
viveksuyal-10 <124768775+viveksuyal-10@users.noreply.github.com>
2024-04-19 05:23:25 +0000

    type conversion

---
+++ 03.js
@@ -0,0 +1,8 @@
+let score = 33;
+console.log(typeof score);
+let finalScore = String(score);
+console.log(typeof finalScore);
+ score = "33ab";
+console.log(typeof score);
+finalScore = Number(score);
+console.log(typeof finalScore);
